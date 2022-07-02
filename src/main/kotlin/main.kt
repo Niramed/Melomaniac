bf2e231c00dc4f556ab0418f5d86491d58c9170d
Niramed <samett@bk.ru>
2022-07-02 09:30:25 +0300

    final

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,18 @@
-fun main () {
-    
+fun main() {
+    val amount = 1001
+    val isRegular = true
+    val discount = 100
+    val discountFirst = 1000
+    val discountSecond = 10_000
+
+    if (amount > discountFirst && amount <= discountSecond) {
+        val resoult = if (isRegular) (amount - discount) * 99 / 100 else amount - discount
+        println("Цена со скидкой $resoult")
+    } else if (amount > discountSecond) {
+        val resoult = if (isRegular) (amount * 95 / 100) * 99 / 100 else amount * 95 / 100
+        println("Цена со скидкой $resoult")
+    } else {
+        val resoult = amount
+        println("Цена без скидки $resoult")
+    }
 }
